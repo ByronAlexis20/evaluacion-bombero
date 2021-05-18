54b4045ea4bfc50f37838b3e9c84d4d6ed3f1c09
ByronAlexis20 <byronalexisramirez20@gmail.com>
2021-05-17 23:10:01 -0500

    preguntas

--- src/com/bombero/control/evaluacion/RegistroPreguntasC.java
+++ src/com/bombero/control/evaluacion/RegistroPreguntasC.java
@@ -1,5 +1,16 @@
 package com.bombero.control.evaluacion;
 
-public class RegistroPreguntasC {
+import java.io.IOException;
 
+import org.zkoss.bind.annotation.AfterCompose;
+import org.zkoss.bind.annotation.ContextParam;
+import org.zkoss.bind.annotation.ContextType;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.Selectors;
+
+public class RegistroPreguntasC {
+	@AfterCompose
+	public void aferCompose(@ContextParam(ContextType.VIEW) Component view) throws IOException{
+		Selectors.wireComponents(view, this, false);
+	}
 }
