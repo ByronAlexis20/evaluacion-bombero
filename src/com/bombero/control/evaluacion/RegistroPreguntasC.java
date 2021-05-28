2e9293af81b10deb8098637c2de3e4a99f920f75
ByronAlexis20 <byronalexisramirez20@gmail.com>
2021-05-28 13:40:11 -0500

    preguntas falta recuperar el listado de preguntas

--- src/com/bombero/control/evaluacion/RegistroPreguntasC.java
+++ src/com/bombero/control/evaluacion/RegistroPreguntasC.java
@@ -1,6 +1,7 @@
 package com.bombero.control.evaluacion;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.zkoss.bind.annotation.AfterCompose;
 import org.zkoss.bind.annotation.Command;
@@ -11,11 +12,22 @@ import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.select.Selectors;
 import org.zkoss.zul.Window;
 
+import com.bombero.model.dao.ModuloDAO;
+import com.bombero.model.dao.PeriodoDAO;
+import com.bombero.model.entity.Modulo;
+import com.bombero.model.entity.Periodo;
+
 public class RegistroPreguntasC {
+	Periodo periodoSeleccionado;
+	Modulo moduloSeleccionado;
+	PeriodoDAO periodoDAO = new PeriodoDAO();
+	ModuloDAO moduloDAO = new ModuloDAO();
+	
 	@AfterCompose
 	public void aferCompose(@ContextParam(ContextType.VIEW) Component view) throws IOException{
 		Selectors.wireComponents(view, this, false);
 	}
+	
 	@Command
 	public void nuevaPregunta() {
 		Window ventanaCargar = (Window) Executions.createComponents("/recursos/forms/evaluacion/preguntaEditar.zul", null, null);
@@ -28,5 +40,23 @@ public class RegistroPreguntasC {
 	@Command
 	public void eliminarPregunta() {
 		
+	}
+	public List<Periodo> getPeriodoLista() {
+		return periodoDAO.buscarActivos();
+	}
+	public List<Modulo> getModuloLista(){
+		return moduloDAO.getModuloPorDescripcion("");
+	}
+	public Periodo getPeriodoSeleccionado() {
+		return periodoSeleccionado;
+	}
+	public void setPeriodoSeleccionado(Periodo periodoSeleccionado) {
+		this.periodoSeleccionado = periodoSeleccionado;
+	}
+	public Modulo getModuloSeleccionado() {
+		return moduloSeleccionado;
+	}
+	public void setModuloSeleccionado(Modulo moduloSeleccionado) {
+		this.moduloSeleccionado = moduloSeleccionado;
 	}
 }
